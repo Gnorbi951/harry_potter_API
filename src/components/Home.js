1a7f1771c4a9e3691c678ad8cfebb2d288501097
MiklosHorvath <horvathmikloswork@gmail.com>
2020-03-30 13:29:46 +0200

    implement home page structure

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hogwarts School of Witchcraft and Wizardry</h1>
+            <button>Check out our Houses</button>
+        </div>
+    )
+}
