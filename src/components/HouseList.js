1c10191e6e123b426feb2602227b463b6e4fb024
Gnorbi951 <gnorbi951@gmail.com>
2020-03-31 10:45:10 +0200

    work on axios calls

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 const HouseList = () => {
   const [key, setKey] = useState(
     "?&key=$2a$10$k64D2VOaGCBynzK6r9E4GeAZKmgXwdSWjJwFdiicclaHlo6EPJmkO"
   );
+  const [housesData, setHouses] = useState([]);
 
   useEffect(() => {
     axios
       .get("https://www.potterapi.com/v1/houses" + key)
-      .then(resp => console.log(resp));
+      .then(resp => {
+        setHouses(resp.data);
+      })
+      .then(console.log(housesData));
   }, []);
 
   return <div>kek</div>;
