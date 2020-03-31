ea37d16a30864680aae931ee60a7fadd0f4060f5
Gnorbi951 <gnorbi951@gmail.com>
2020-03-31 10:31:18 +0200

    get api houses data

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function HouseList() {
+const HouseList = () => {
+  const [key, setKey] = useState(
+    "?&key=$2a$10$k64D2VOaGCBynzK6r9E4GeAZKmgXwdSWjJwFdiicclaHlo6EPJmkO"
+  );
+
+  useEffect(() => {
+    axios
+      .get("https://www.potterapi.com/v1/houses" + key)
+      .then(resp => console.log(resp));
+  }, []);
+
   return <div>kek</div>;
-}
+};
 
 export default HouseList;
