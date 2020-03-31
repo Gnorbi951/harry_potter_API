36d1d77d2d3f2ab1e3b65fd844a2d1defcd26602
Evelin Szabados <szabadosevelin7@gmail.com>
2020-03-31 13:34:38 +0200

    Fix issue with fetching professor data

--- src/components/Professors.js
+++ src/components/Professors.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import axios from "axios";
 const ProfContainer = styled.div`
   margin: 6rem auto;
 `;
@@ -8,24 +9,32 @@ const Professors = () => {
   const [professors, setProfessors] = useState([]);
 
   useEffect(() => {
-    let professorList = [];
-    fetch(
-      "https://www.potterapi.com/v1/characters?&key=$2a$10$k64D2VOaGCBynzK6r9E4GeAZKmgXwdSWjJwFdiicclaHlo6EPJmkO"
-    )
-      .then(res => res.json())
-      .then(fetchedResult => {
-        fetchedResult.filter(character => {
+    axios
+      .get(
+        "https://www.potterapi.com/v1/characters?&key=$2a$10$k64D2VOaGCBynzK6r9E4GeAZKmgXwdSWjJwFdiicclaHlo6EPJmkO"
+      )
+      .then(res => {
+        const professorData = res.data;
+        let professorList = [];
+        professorData.filter(character => {
           if (
             String(character.role).includes("Professor") &&
             String(character.school).includes("Hogwarts")
           ) {
             professorList.push(character);
+            console.log(character.name);
           }
         });
+        setProfessors(professorList);
       });
-    setProfessors(professorList);
   }, []);
 
-  return <ProfContainer></ProfContainer>;
+  return (
+    <React.Fragment>
+      {professors.map(prof => (
+        <p key={prof.id}>{prof.name}</p>
+      ))}
+    </React.Fragment>
+  );
 };
 export default Professors;
