c83429347e45c638a08cd4d22ce0b4dbfefd8d5d
Evelin Szabados <szabadosevelin7@gmail.com>
2020-03-31 12:01:57 +0200

    Fetch data for professors

--- src/components/Professors.js
+++ src/components/Professors.js
@@ -1,35 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-
 const ProfContainer = styled.div`
-margin: 6rem auto;
-`
+  margin: 6rem auto;
+`;
 
 const Professors = () => {
-    const [professors, setProfessors] = useState([]);
-    
-    useEffect(() => {
-        fetch('https://www.potterapi.com/v1/characters?&key=$2a$10$k64D2VOaGCBynzK6r9E4GeAZKmgXwdSWjJwFdiicclaHlo6EPJmkO')
-        .then(res => res.json())
-        .then( fetchedResult => {fetchedResult.filter(character => {
-            if(String(character.role).includes("Professor")&&String(character.school).includes("Hogwarts")){
-                setProfessors(...professors, character);
-            }
-            
-        }
-      
-        )})
-    },[]);
+  const [professors, setProfessors] = useState([]);
 
-    return (
-        <ProfContainer>
-            
-        </ProfContainer>
+  useEffect(() => {
+    fetch(
+      "https://www.potterapi.com/v1/characters?&key=$2a$10$k64D2VOaGCBynzK6r9E4GeAZKmgXwdSWjJwFdiicclaHlo6EPJmkO"
     )
-}
+      .then(res => res.json())
+      .then(fetchedResult => {
+        fetchedResult.filter(character => {
+          if (
+            String(character.role).includes("Professor") &&
+            String(character.school).includes("Hogwarts")
+          ) {
+          }
+        });
+      });
+  }, [professors]);
 
-
-
+  return <ProfContainer></ProfContainer>;
+};
 export default Professors;
-
-//data.filter(x => String(x.approval).includes(approvalVariable));
