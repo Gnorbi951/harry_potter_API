0aaca1fbdb22fe68cf95acaab67abe567177500c
Gnorbi951 <gnorbi951@gmail.com>
2020-03-31 11:26:01 +0200

    Add minimal style

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -3,6 +3,29 @@ import axios from "axios";
 import styled from "styled-components";
 import NavBar from "./NavBar";
 
+const CardContainer = styled.div`
+  background-color: 323232;
+`;
+
+const Card = styled.div`
+  position: relative;
+
+  display: inline-block;
+  margin: 5px;
+  margin-top: 1rem;
+  margin-bottom: 20px;
+  padding: 24px;
+
+  border-style: solid;
+  border-radius: 2px;
+  background: grey;
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
+
+  border-radius: 2rem;
+  background-color: #ff1e56;
+`;
+
 const HouseList = () => {
   const [key, setKey] = useState(
     "?&key=$2a$10$k64D2VOaGCBynzK6r9E4GeAZKmgXwdSWjJwFdiicclaHlo6EPJmkO"
@@ -25,11 +48,13 @@ const HouseList = () => {
 
   return (
     <React.Fragment>
-      {housesData.map(data => (
-        <div key={data.name}>
-          <h1>{data.name}</h1>
-        </div>
-      ))}
+      <CardContainer>
+        {housesData.map(data => (
+          <Card id={data.name} key={data.name}>
+            <h1>{data.name}</h1>
+          </Card>
+        ))}
+      </CardContainer>
     </React.Fragment>
   );
 };
