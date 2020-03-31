2332131363d341b74419c6f90ceabbe1d2d8b487
Evelin Szabados <szabadosevelin7@gmail.com>
2020-03-31 14:44:40 +0200

    Display basic data in cards for professors

--- src/components/subcomponents/ProfessorsDetail.js
+++ src/components/subcomponents/ProfessorsDetail.js
@@ -1,11 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import ProfilePic from "/home/evelin/Codecool/Advanced/potter/src/wizard.png";
 
-const Card = styled.div`
-  background-color: dodgerblue;
-  color: white;
+const CardBody = styled.div`
+  /* margin: 1rem auto; */
+  background-color: #ccc;
   padding: 1rem;
-  height: 4rem;
+  width: inherit;
+`;
+const Card = styled.div`
+  color: #404040;
+  /* padding: 1rem; */
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  border: 1px solid #ccc;
+`;
+
+const Profile = styled.img`
+  margin: 0 auto;
+  min-width: 12rem;
+  max-width: 15rem;
 `;
 
 const Cards = styled.div`
@@ -13,14 +28,27 @@ const Cards = styled.div`
   margin: 3rem auto;
   padding: 1.3rem;
   display: grid;
-  grid-gap: 1rem;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 1.6rem;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
 `;
+
 const ProfessorsDetail = props => {
   return (
     <Cards>
       {props.data.map(profData => (
-        <Card>{profData.name}</Card>
+        <Card>
+          <Profile src={ProfilePic} alt="Wizard profile"></Profile>
+          <CardBody>
+            <p>{profData.name}</p>
+            <p>
+              {profData.dumbledoresArmy
+                ? "In dumbledore's army: yes"
+                : "In dumbledore's army: no"}
+            </p>
+            <p>Blood status: {profData.bloodStatus}</p>
+            <p>Species: {profData.species}</p>
+          </CardBody>
+        </Card>
       ))}
     </Cards>
   );
