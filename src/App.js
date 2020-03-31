455075fa093bb25f6709d134b6f8ff91e6b2779a
Gnorbi951 <gnorbi951@gmail.com>
2020-03-31 11:00:02 +0200

    resolve merge conflict correctly this time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
+import HouseList from "./components/HouseList";
 // import NavBar from "./components/NavBar";
 import "./App.css";
 
