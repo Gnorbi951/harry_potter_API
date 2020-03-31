efa99c4978f0e8c8d06bc0d46f90319bd717f2c0
Evelin Szabados <szabadosevelin7@gmail.com>
2020-03-31 10:50:58 +0200

    Fix issue with dev

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-import Professors from "./components/Professors"
 
 // import NavBar from "./components/NavBar";
 import "./App.css";
@@ -12,8 +10,6 @@ const App = () => {
     <React.Fragment>
       <Router>
         <Route exact path="/" component={Home}></Route>
-        <Route exact path="/professors" component={NavBar}></Route>
-        <Route exact path="/professors" component={Professors}></Route>
       </Router>
     </React.Fragment>
   );
