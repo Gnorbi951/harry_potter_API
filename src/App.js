87ab27a85d0a095b36fc67b05c127838d2d047d4
Bence Losonczy <losonzybence@gmail.com>
2020-04-01 18:57:22 +0200

    rethink the speech command

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 import HouseList from "./components/House/HouseList";
@@ -9,31 +9,59 @@ import Professors from "./components/Professors";
 import { ProfessorsProvider } from "./context/ProfessorsContext";
 import SortToHouse from "./components/SortToHouse";
 import { SortProvider } from "./context/SortContext";
-import MuggleDetector from "./components/MuggleDetector";
+import { useSpeechRecognition } from "react-speech-kit";
 
-const App = () => {
+function placeHolder() {
+  return <React.Fragment>nono</React.Fragment>;
+}
+
+function returnFunct() {
   return (
     <React.Fragment>
-      <Router>
-        <Route path="/muggleDetector" component={MuggleDetector}></Route>
-        <Route exact path="/" component={Home}></Route>
-        <HouseProvider>
-          <Route
-            path="/(professors|houses|sortingHat)"
-            component={NavBar}
-          ></Route>
-          <Route exact path="/houses" component={HouseList}></Route>
-          <Route path="/houses/:id" component={SingleHouse}></Route>
-        </HouseProvider>
-        <SortProvider>
-          <Route exact path="/sortingHat" component={SortToHouse}></Route>
-        </SortProvider>
-        <ProfessorsProvider>
-          <Route exact path="/professors" component={Professors}></Route>
-        </ProfessorsProvider>
-      </Router>
+      <Route exact path="/" component={Home}></Route>
+      <HouseProvider>
+        <Route
+          path="/(professors|houses|sortingHat)"
+          component={NavBar}
+        ></Route>
+        <Route exact path="/houses" component={HouseList}></Route>
+        <Route path="/houses/:id" component={SingleHouse}></Route>
+      </HouseProvider>
+      <SortProvider>
+        <Route exact path="/sortingHat" component={SortToHouse}></Route>
+      </SortProvider>
+      <ProfessorsProvider>
+        <Route exact path="/professors" component={Professors}></Route>
+      </ProfessorsProvider>
     </React.Fragment>
   );
-};
+}
 
+function App() {
+  const [isMuggle, setIsMuggle] = useState();
+  const [result, setResult] = useState();
+  const { listen, listening, stop } = useSpeechRecognition({
+    onResult: result => setResult(result)
+  });
+
+  function setMuggle() {
+    // {result = "kutya" ? setIsMuggle(false) : setIsMuggle(true) }
+    // if (result !== "kutya") {
+    //   setIsMuggle(true);
+    // } else {
+    //   setIsMuggle(false);
+    // }
+    console.log(result);
+  }
+
+  return (
+    <div>
+      {listening ? "Speak, I'm listening" : ""}
+      <textarea value={result} />
+      <button onClick={listen}>Listen</button>
+      <button onClick={stop}>Stop</button>
+      {setMuggle()}
+    </div>
+  );
+}
 export default App;
--- src/components/MuggleDetector.js
+++ src/components/MuggleDetector.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useSpeechRecognition } from "react-speech-kit";
+import { Link } from "react-router-dom";
 
 const MuggleDetector = () => {
   const [result, setResult] = useState();
