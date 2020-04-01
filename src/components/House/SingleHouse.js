b3d37dd701770061f7d0f3f2365c6e095690407b
Gnorbi951 <gnorbi951@gmail.com>
2020-04-01 12:45:00 +0200

    Start axios for fetching data

--- src/components/House/SingleHouse.js
+++ src/components/House/SingleHouse.js
@@ -1,23 +1,36 @@
 import React, { useContext, useState, useEffect } from "react";
-import { HouseContext } from "../../context/HouseContext";
+import { HouseContext, APIKey } from "../../context/HouseContext";
 import axios from "axios";
 
 const SingleHouse = props => {
   const { houses } = useContext(HouseContext);
   const [currentHouse, setCurrentHouse] = useState({});
+  const [membersId, setMembersId] = useState([]);
+  const [members, setMembers] = useState([{}]);
 
   useEffect(() => {
     let house = houses.find(house => house._id === props.match.params.id);
     if (!house) {
       axios
         .get(
-          `https://www.potterapi.com/v1/houses/${props.match.params.id}?&key=$2a$10$k64D2VOaGCBynzK6r9E4GeAZKmgXwdSWjJwFdiicclaHlo6EPJmkO`
+          `https://www.potterapi.com/v1/houses/${props.match.params.id}${APIKey}`
         )
         .then(house => setCurrentHouse(house.data));
     } else {
       setCurrentHouse(house);
+      setMembersId(house.members);
     }
   }, [houses, props.match.params.id]);
+
+  useEffect(() => {
+    membersId.map(_id => {
+      axios
+        .get(`https://www.potterapi.com/v1/characters/${_id}/${APIKey}`)
+        .then(resp => {
+          setMembers(...members, resp.data);
+        });
+    });
+  }, [membersId]);
 
   return (
     <React.Fragment>
@@ -27,6 +40,11 @@ const SingleHouse = props => {
       <h3>Founder: {currentHouse.founder}</h3>
 
       <h1>Members</h1>
+      <ul>
+        {membersId.map(element => (
+          <li>{element}</li>
+        ))}
+      </ul>
     </React.Fragment>
   );
 };
