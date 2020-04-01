8bac32f7b7b953ca615a6653d7f7c92579c1ffd1
Bence Losonczy <losonzybence@gmail.com>
2020-04-01 16:08:04 +0200

    recreating sort background

--- src/components/SortToHouse.js
+++ src/components/SortToHouse.js
@@ -1,5 +1,21 @@
 import React, { useContext } from "react";
 import { SortContext } from "../context/SortContext";
+import styled from "styled-components";
+import Background from "../sortingBackGround.jpg";
+
+const BackGround = styled.div`
+  background-image: url(${Background});
+  background-position: center;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  background-size: cover;
+  margin: 0;
+`;
 
 const SortToHouse = () => {
   const { sortHouses } = useContext(SortContext);
@@ -11,9 +27,9 @@ const SortToHouse = () => {
   };
 
   return (
-    <div>
+    <BackGround>
       <button onClick={() => sortToHouse()}>Sorting Hat</button>
-    </div>
+    </BackGround>
   );
 };
 export default SortToHouse;
