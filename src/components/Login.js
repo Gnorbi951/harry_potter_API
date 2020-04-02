4325abc9f6e03cbbefc9f771a46ee5c2526998ab
Gnorbi951 <gnorbi951@gmail.com>
2020-04-02 11:51:52 +0200

    fix page reload

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,13 +7,15 @@ import App from "../App";
 const Login = props => {
   const [result, setResult] = useState();
   const { validLogin, setValidLogin } = useContext(LoginContext);
-
+  let loggedIn = false; // This is for preventing page reload because it's buggy in a functional component
   const { listen, listening, stop } = useSpeechRecognition({
     onResult: result => {
       setResult(result);
-      if (result === "alohomora") {
+      if (result === "alohomora" && loggedIn === false) {
         setValidLogin(true);
         props.history.push("/");
+        stop();
+        loggedIn = true;
       }
     }
   });
