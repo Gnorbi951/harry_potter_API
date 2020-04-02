dae4b5cbf9b132ead628d8e8cf248371b6d932f2
MiklosHorvath <horvathmikloswork@gmail.com>
2020-04-02 10:48:46 +0200

    implements sending form data to backend

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+import axios from "axios";
+
+
+const OwlImage = styled.img`
+    margin: 0 auto;
+    min-width: 350px;
+    max-height: 450px;
+`;
+
 
 const Application = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        axios({
+            method: "POST", 
+            url:"http://localhost:8888/application", 
+            data: {
+                name: name,   
+                email: email                
+            }
+        }).then((response)=>{
+            if (response.data.msg === 'success'){
+                alert("Message Sent."); 
+                this.resetForm()
+            }else if(response.data.msg === 'fail'){
+                alert("Message failed to send.")
+            }
+        })
+    }
+
     return (
-        <div>
-            test
-        </div>
+        <React.Fragment>
+            <form  onSubmit={handleSubmit} method="POST">
+                <label>Applicant's name: </label>
+                <input type="text" id="name"></input>
+                <label>Email: </label>
+                <input type="email" id="email"></input>
+                <button type="submit">submit application</button>
+            </form>
+            <OwlImage src='./subcomponents/images/owl.jpg'></OwlImage>
+        </React.Fragment>
     )
 }
 
