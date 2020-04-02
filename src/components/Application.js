4b72779d8f56a207979ff5d364f16c62aa869fad
Evelin Szabados <szabadosevelin7@gmail.com>
2020-04-02 14:18:40 +0200

    Implement base design for application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,49 +1,97 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 import axios from "axios";
-
+import { Owl } from "./subcomponents/KeyFrames";
 
 const OwlImage = styled.img`
-    margin: 0 auto;
-    min-width: 350px;
-    max-height: 450px;
+  margin: 0 auto;
+  max-width: 300px;
+  animation: ${Owl} 3s forwards 0s ease;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  animation: ${Owl} 3s forwards 0s ease;
+`;
+const Input = styled.input`
+  width: 50%;
+  padding: 1rem;
+  margin: 1rem;
+  border-radius: 20px;
+  border: 1px solid #ccc;
+  &:focus {
+    outline: none;
+  }
+`;
+const FieldContainer = styled.div`
+  margin: 5rem auto;
+  text-align: center;
+  width: 60%;
+`;
+
+const Submit = styled.button`
+  border: none;
+  background-color: #843a24;
+  color: white;
+  padding: 1rem;
+  font-size: 18px;
+  border-radius: 10px;
+  animation: ${Owl} 3s forwards 0s ease;
+  cursor: pointer;
+  &:hover {
+    background-color: #59231b;
+  }
+`;
 
 const Application = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        axios({
-            method: "POST", 
-            url:"http://localhost:8888/application", 
-            data: {
-                name: name,   
-                email: email                
-            }
-        }).then((response)=>{
-            if (response.data.msg === 'success'){
-                alert("Message Sent."); 
-                this.resetForm()
-            }else if(response.data.msg === 'fail'){
-                alert("Message failed to send.")
-            }
-        })
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    axios({
+      method: "POST",
+      url: "http://localhost:8888/application",
+      data: {
+        name: name,
+        email: email
+      }
+    }).then(response => {
+      if (response.data.msg === "success") {
+        alert("Message Sent.");
+        this.resetForm();
+      } else if (response.data.msg === "fail") {
+        alert("Message failed to send.");
+      }
+    });
+  };
 
-    return (
-        <React.Fragment>
-            <form  onSubmit={handleSubmit} method="POST">
-                <label>Applicant's name: </label>
-                <input type="text" id="name"></input>
-                <label>Email: </label>
-                <input type="email" id="email"></input>
-                <button type="submit">submit application</button>
-            </form>
-            <OwlImage src='./subcomponents/images/owl.jpg'></OwlImage>
-        </React.Fragment>
-    )
-}
+  return (
+    <FieldContainer>
+      <OwlImage
+        src={require("./subcomponents/images/owl.jpg")}
+        alt="Owl"
+      ></OwlImage>
+      <Form onSubmit={handleSubmit} method="POST">
+        <Input
+          type="text"
+          id="name"
+          placeholder="Name"
+          autoComplete="off"
+        ></Input>
+
+        <Input
+          type="email"
+          id="email"
+          placeholder="E-mail"
+          autoComplete="off"
+        ></Input>
+        <Submit type="submit">submit application</Submit>
+      </Form>
+    </FieldContainer>
+  );
+};
 
 export default Application;
--- src/components/subcomponents/KeyFrames.js
+++ src/components/subcomponents/KeyFrames.js
@@ -10,3 +10,14 @@ export const keyFrameMainPage = keyframes`
         opacity: 1;
     }
   `;
+
+export const Owl = keyframes`
+0% {
+     transform: translateY(0);
+     opacity: 0;
+ }
+ 100% {
+     transform: rotate(0) translateY(0);
+     opacity: 1;
+ }
+`;
