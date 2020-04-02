3fa4291aca3053bc860376b3b4940e0d99a66e61
Gnorbi951 <gnorbi951@gmail.com>
2020-04-02 11:33:05 +0200

    login is set to true for kutya

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useSpeechRecognition } from "react-speech-kit";
 import { LoginContext } from "../context/LoginContext";
 
 const Login = () => {
+  const [result, setResult] = useState();
   const { validLogin, setValidLogin } = useContext(LoginContext);
 
   const { listen, listening, stop } = useSpeechRecognition({
-    onResult: result => setValidLogin([result])
+    onResult: result => setResult(result)
   });
+
+  useEffect(() => {
+    if (result === "kutya") {
+      setValidLogin(true);
+    }
+  }, [result]);
 
   return (
     <div>
       {listening ? "Speak, I'm listening" : ""}
-      <textarea value={validLogin} />
+      <textarea value={result} />
       <button onClick={listen}>Listen</button>
       <button onClick={stop}>Stop</button>
     </div>
