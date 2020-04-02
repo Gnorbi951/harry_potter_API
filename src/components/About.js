868294afd82b633f8c741359b9ba56a7cd7077e6
Evelin Szabados <szabadosevelin7@gmail.com>
2020-04-02 16:10:28 +0200

    Implement about design

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  margin: 2rem;
+  margin: 2rem auto;
   padding: 1rem;
+  width: 50%;
+  border-radius: 20px;
+  border-top: 4px solid #278e81;
+  border-bottom: 4px solid #278e81;
 `;
 
 const Header = styled.h1`
   font-size: 4rem;
+  color: #278e81;
 `;
 
 const Link = styled.div`
@@ -17,7 +22,16 @@ const Link = styled.div`
 `;
 
 const Creator = styled.li`
-  margin-top: 0.5rem;
+  margin: 0.5rem;
+  list-style: none;
+  padding: 0.5rem;
+  border-bottom: 1px solid #ccc;
+`;
+
+const CustomLink = styled.a`
+  text-decoration: none;
+  color: teal;
+  font-weight: bold;
 `;
 
 const About = () => {
@@ -32,13 +46,15 @@ const About = () => {
           <Creator>Bence Losonczy</Creator>
           <Creator>Miklós Horváth</Creator>
         </ul>
-        <Link>
-          <a href="https://github.com/Gnorbi951/harry_potter_API">
+        <Link style={{ textDecoration: "none", margin: "0" }}>
+          <CustomLink href="https://github.com/Gnorbi951/harry_potter_API">
             Link to github repository
-          </a>
+          </CustomLink>
         </Link>
-        <Link>
-          <a href="https://www.potterapi.com/">The API we used</a>
+        <Link style={{ textDecoration: "none", margin: "0" }}>
+          <CustomLink href="https://www.potterapi.com/">
+            The API we used
+          </CustomLink>
         </Link>
       </Container>
     </>
